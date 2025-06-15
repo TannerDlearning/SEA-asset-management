abba224522c1d7de21a5fcf0e4689c5745b92643
TannerD <tannerdlearning2005@gmail.com>
2025-06-15 10:01:04 +0100

    Register blueprints and run app

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import auth, assets
 
 app = Flask(__name__)
 app.secret_key = 'dev'
+
+app.register_blueprint(auth.bp)
+app.register_blueprint(assets.bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
